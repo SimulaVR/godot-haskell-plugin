a74cf14a7d778e4a69881b7d29f011b4e6db401d
David Kraeutmann <kane@kane.cx>
2018-04-08 19:47:21 -0400

    Update library ver

--- flib/Flib.hs
+++ flib/Flib.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface, TypeApplications #-}
 module Flib where
 
 import Foreign
-import Foreign.C
 import Godot.Gdnative
 
 import Data.IORef
+import Data.Word
 import System.IO.Unsafe
 
 apiRef :: IORef GodotGdnativeCoreApiStruct
@@ -31,7 +31,14 @@ godot_nativescript_init :: Ptr () -> IO ()
 godot_nativescript_init desc = do
   putStrLn "nativescript init"
   api <- readIORef apiRef
-  str <- withCWStringLen "foo" $ \(ptr,len) -> godot_string_new_with_wide_string api ptr (fromIntegral len)
-  godot_print api str
-  
+  color <- godot_color_new_rgba api 0 1 0 0.5
+  putStr "getting r (should be 0): "
+  godot_color_get_r api color >>= print
+  putStr "getting g (should be 1): "
+  godot_color_get_g api color >>= print
+  putStr "getting b (should be 0): "
+  godot_color_get_b api color >>= print
+  putStr "getting a (should be 0.5): "
+  godot_color_get_a api color >>= print
+
 foreign export ccall godot_nativescript_init :: Ptr () -> IO ()
