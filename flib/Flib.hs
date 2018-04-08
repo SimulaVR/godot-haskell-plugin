62a7885f1d3b487fb923b3f61dffc5984e5508a4
David Kraeutmann <kane@kane.cx>
2018-04-08 19:33:55 -0400

    Update

--- flib/Flib.hs
+++ flib/Flib.hs
@@ -2,10 +2,23 @@
 module Flib where
 
 import Foreign
+import Foreign.C
 import Godot.Gdnative
 
+import Data.IORef
+import System.IO.Unsafe
+
+apiRef :: IORef GodotGdnativeCoreApiStruct
+apiRef = unsafePerformIO (newIORef undefined)
+{-# NOINLINE apiRef #-}
+
 godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
-godot_gdnative_init optPtr = putStrLn "gdnative init"
+godot_gdnative_init optPtr = do
+  putStrLn "gdnative init"
+  opt <- peek optPtr
+  let api = gdnativeInitOptionsApiStruct opt
+  writeIORef apiRef api
+
 
 foreign export ccall godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
 
@@ -15,6 +28,10 @@ godot_gdnative_terminate optPtr = putStrLn "gdnative terminate"
 foreign export ccall godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPtr -> IO ()
 
 godot_nativescript_init :: Ptr () -> IO ()
-godot_nativescript_init desc = putStrLn "nativescript init"
-
+godot_nativescript_init desc = do
+  putStrLn "nativescript init"
+  api <- readIORef apiRef
+  str <- withCWStringLen "foo" $ \(ptr,len) -> godot_string_new_with_wide_string api ptr (fromIntegral len)
+  godot_print api str
+  
 foreign export ccall godot_nativescript_init :: Ptr () -> IO ()
