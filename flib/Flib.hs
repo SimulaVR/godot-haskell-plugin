0aff2bd1cdaac1090669cfda2ec8f7feebc7fc3e
David Kraeutmann <kane@kane.cx>
2018-06-04 09:19:50 -0400

    godot-haskell bump

--- flib/Flib.hs
+++ flib/Flib.hs
@@ -2,23 +2,18 @@
 module Flib where
 
 import Foreign
-import Godot.Gdnative
+import Godot.Gdnative.Internal
 
 import Data.IORef
 import Data.Word
 import System.IO.Unsafe
 
-apiRef :: IORef GodotGdnativeCoreApiStruct
-apiRef = unsafePerformIO (newIORef undefined)
-{-# NOINLINE apiRef #-}
 
 godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
 godot_gdnative_init optPtr = do
   putStrLn "gdnative init"
   opt <- peek optPtr
-  let api = gdnativeInitOptionsApiStruct opt
-  writeIORef apiRef api
-
+  initApiStructs
 
 foreign export ccall godot_gdnative_init :: GodotGdnativeInitOptionsPtr -> IO ()
 
@@ -30,15 +25,5 @@ foreign export ccall godot_gdnative_terminate :: GodotGdnativeTerminateOptionsPt
 godot_nativescript_init :: Ptr () -> IO ()
 godot_nativescript_init desc = do
   putStrLn "nativescript init"
-  api <- readIORef apiRef
-  color <- godot_color_new_rgba api 0 1 0 0.5
-  putStr "getting r (should be 0): "
-  godot_color_get_r api color >>= print
-  putStr "getting g (should be 1): "
-  godot_color_get_g api color >>= print
-  putStr "getting b (should be 0): "
-  godot_color_get_b api color >>= print
-  putStr "getting a (should be 0.5): "
-  godot_color_get_a api color >>= print
 
 foreign export ccall godot_nativescript_init :: Ptr () -> IO ()
